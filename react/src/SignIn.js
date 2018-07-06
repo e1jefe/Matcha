3fac21c15456fb5fc4c3f302702d7b549e367557
e1jefe <dmitry.sheptun@gmail.com>
2018-07-06 16:04:09 +0300

    signi js

---
+++ react/src/SignIn.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import './SignIn.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+var Input = React.createClass({
+	render: function() {
+		return (
+			<div className="Input">
+				<input 
+					id={this.props.id}
+					autoComplete="false"
+					required
+					type={this.props.type}
+					placeholder={this.props.placeholder}
+				/>	
+				<label htmlFor={this.props.id}></label>
+			</div>
+		);
+	}
+});
+
+var Modal = React.createClass({
+	render: function() {
+		return (
+			<div className="Modal">
+				<form 
+					onSubmit={this.props.onSubmit}
+					className="ModalForm">
+					<Input
+						id="name"
+						type="text"
+						placeholder="login" />
+					<Input
+						id="password"
+						type="password"
+						placeholder="password" />
+
+					<button>
+						Log in <i className="fa fa-fw fa-chevron-right"></i>
+					</button>
+					<p className="message">Not registered? <a href="">Create your account</a></p>
+					<p className="message"><a href="">Forgot password?</a></p>
+				</form>
+			</div>
+		);
+	}
+});
+
+var SignIn = React.createClass({
+	
+	getInitialState: function() {
+		return { mounted: false };
+	},
+	
+	componentDidMount: function() {
+		this.setState({ mounted: true });
+	},
+	
+	handleSubmit: function(e) {
+		this.setState({ mounted: false });
+		e.preventDefault();
+	},
+
+	render: function() {
+		var child;
+
+		if(this.state.mounted) {
+			child = (<Modal onSubmit={this.handleSubmit} />);
+		}
+		
+		return(
+			<div className="SignIn">
+				<ReactCSSTransitionGroup 
+					transitionName="example"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={300}>
+						{child}
+				</ReactCSSTransitionGroup>
+			</div>
+		);
+	}
+});
+
+export default SignIn;
---
+++ react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignIn from './SignIn';
+import './index.css';
+
+ReactDOM.render(
+  <SignIn />,
+  document.getElementById('root')
+);
