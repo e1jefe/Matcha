4e067a5122575c0b268336763d3dc122618841bc
bertafly <irrina.novikova@gmail.com>
2018-07-12 17:10:45 +0300

    added app

---
+++ www/app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './user/main/components/headerComponents/Header.js';
+import Home from './user/home/Home.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
