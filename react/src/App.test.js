dfa36d8d39ec445e889c79e651b542fd8edf5200
bertafly <irrina.novikova@gmail.com>
2018-07-17 00:40:54 +0300

    deleted otter nodemodules and react folder

--- react/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../../www/app/src/user/main/components/headerComponents/SigninForm';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
