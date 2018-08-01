73318b9ac64c7057b13db1c07105aac1d196c39b
e1jefe <dmitry.sheptun@gmail.com>
2018-08-01 21:33:13 +0300

    some changes search

--- www/app/src/user/home/SearchComponents.jsx
+++ www/app/src/user/home/SearchComponents.jsx
@@ -6,21 +6,47 @@ import { Slider, Switch } from 'antd';
 import { Button } from 'antd';
 import EditableTagGroup from './TagComponents.jsx';
 import UsersCards from "./UsersCards";
+import jwtDecode from 'jwt-decode';
+import { PostData } from '../main/components/PostData';
 
 class SearchComponents extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            author: true,
-            userLogin: props.login
-        };
+            login: props.login,
+            userId: props.userId,
+            fullProfile: props.access,
+        }
+        // this.handleSubmitInfo = this.handleSubmitInfo.bind(this)
+        // this.handleSubmitAbout = this.handleSubmitAbout.bind(this)
     }
-    state = {
-        disabled: false,
-    };
 
-    handleDisabledChange = (disabled) => {
-        this.setState({disabled});
+    componentWillMount() {
+        const token = localStorage.getItem('token')
+        if (token !== null)
+        {
+            let user = jwtDecode(token)
+            if (user.userLogin !== '')
+                user = user.userId
+            PostData('user/search', {userId: user}).then ((result) => {
+                this.setState({
+                    // Age: result.userData.age,
+                    // Distance: result.userData.distance,
+                    // bio: result.userData.bio,
+                    // fameRate: result.userData.fameRate,
+                    // tags: result.userData.tags,
+                    // avatar: result.userData.profilePic,
+                    // whoLikedMe: result.whoLikesUser
+                })
+            })
+        }
+    }
+
+    // handleDisabledChange = (disabled) => {
+    //     this.setState({disabled});
+    // }
+    onChange(value) {
+        console.log('onChange: ', value);
     }
 
     render() {
@@ -31,11 +57,11 @@ class SearchComponents extends Component {
             <div id="wrapper1">
                 <div className="sliders">
                 <p>Age</p>
-                <Slider  min={18} range defaultValue={[18, 40]} />
+                <Slider  min={18} range defaultValue={[18, 40]} onChange={this.onChange}/>
                 <p>Distance</p>
-                <Slider   min={2} defaultValue={30}  />
+                <Slider   min={2} defaultValue={30}  onChange={this.onChange}/>
                 <p>Fame rating</p>
-                <Slider range step={10} defaultValue={[10, 50]} />
+                <Slider range step={10} defaultValue={[10, 50]} onChange={this.onChange}/>
                 </div>
                 <div className="tags">
                 <EditableTagGroup />
--- www/app/src/user/home/UsersCards.jsx
+++ www/app/src/user/home/UsersCards.jsx
@@ -26,30 +26,7 @@ class UsersCards extends Component {
                     <CardLink href="#">Block user</CardLink>
                 </CardBody>
             </Card></Col>
-                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>  <Card>
-                        <CardBody>
-                            <CardTitle><NavLink to="/userID">Olga Virchenko, </NavLink> 34</CardTitle>
-                            <CardSubtitle>Ukraine, Kyiv</CardSubtitle>
-                        </CardBody>
-                        <NavLink to="/userID"><img src="https://scontent.fiev11-1.fna.fbcdn.net/v/t1.0-9/33900375_2108633922735576_1234385064138113024_n.jpg?_nc_cat=0&oh=50f4018fc45f729fdfdd9026e80206a3&oe=5C084579" /> </NavLink>
-                        <CardBody>
-                            <CardText>About me...</CardText>
-                            <Button type="danger">Like</Button>
-                            <CardLink href="#">Block user</CardLink>
-                        </CardBody>
-                    </Card></Col>
-                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>  <Card>
-                        <CardBody>
-                            <CardTitle><NavLink to="/userID">Olga Virchenko, </NavLink> 34</CardTitle>
-                            <CardSubtitle>Ukraine, Kyiv</CardSubtitle>
-                        </CardBody>
-                        <NavLink to="/userID"><img src="https://scontent.fiev11-1.fna.fbcdn.net/v/t1.0-9/33900375_2108633922735576_1234385064138113024_n.jpg?_nc_cat=0&oh=50f4018fc45f729fdfdd9026e80206a3&oe=5C084579" /> </NavLink>
-                        <CardBody>
-                            <CardText>About me...</CardText>
-                            <Button type="danger">Like</Button>
-                            <CardLink href="#">Block user</CardLink>
-                        </CardBody>
-                    </Card></Col>
+
                     <Col xs={24} sm={12} md={12} lg={12} xl={12}>  <Card>
                         <CardBody>
                             <CardTitle><NavLink to="/userID">Olga Virchenko, </NavLink> 34</CardTitle>
