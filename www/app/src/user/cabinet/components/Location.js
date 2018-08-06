526450fd5a41c5874e30f22329850806ce07ada1
Berta Fly <inovykov@e1r6p5.unit.ua>
2018-08-06 21:36:18 +0300

    changing my location at profile on map

--- www/app/src/user/cabinet/components/Location.js
+++ www/app/src/user/cabinet/components/Location.js
@@ -7,9 +7,40 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 import axios from 'axios'
 
-Geocode.setApiKey('AIzaSyCP9L40mEFKlERXq8cHeG_x0EZfsWW9A-s')
+Geocode.setApiKey('AIzaSyBj7XDClRGcxA9xTV3KPIwyijuHODynh4w')
 Geocode.enableDebug();
 // const AnyReactComponent = ({ text }) => <div>{text}</div>
+// const MyGoogleMap = compose(
+// 	withProps({
+// 		googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+// 		loadingElement: <div style={{ height: `100%` }} />,
+// 		containerElement: <div style={{ height: `400px` }} />,
+// 		mapElement: <div style={{ height: `100%` }} />,
+
+// 	}),
+// 	lifecycle({
+// 		componentWillMount(){
+// 			const refs = {}
+// 			this.setState({
+// 				position: null,
+// 				onMarkerMount: ref => {
+// 					refs.marker = ref;
+// 				},
+// 				onPositionChanged: () => {
+// 					const position = refs.marker.getPosition()
+// 					console.log(position.toString())
+// 				}
+// 			})
+// 		},
+// 	}),
+// 	withScriptjs,
+// 	withGoogleMap)((props) => 
+// 		<GoogleMap defaultZoom={11}	defaultCenter={{ lat: 50.469017, lng: 30.462191 }}>
+// 			{props.isMarkerShown && <Marker
+// 					position={{ lat: parseInt(props.lat), lng: parseInt(props.lng)}}
+// 					draggable={true} ref={props.onMarkerMount} onPositionChanged={props.onPositionChanged} />}
+// 		</GoogleMap>
+// 	)
 
 
 class Location extends Component {
@@ -22,53 +53,132 @@ class Location extends Component {
 			longAllow: '',
 			latAllow: '',
 			city: '',
-			country: ''
+			country: '',
+			markers: {
+				position: {
+					lat: 0,
+					lng: 0
+				},
+				key: Date.now(),
+			},
+			mapCenter: { lat: 50.469017, lng: 30.462191 },
+			mapRef: null,
+			zoomLevel: 13
 		}
 	}
 
 	componentDidMount(){
+		PostData('user/getCoord', {uId: this.state.uId}).then ((result) => {
+			this.setState({
+				markers: {
+					position: {
+						lat: parseFloat(result.latAllow),
+						lng: parseFloat(result.lngAllow)
+					}
+				}
+			})
+		})
 		if(navigator.geolocation){
 	        console.log("in allow coord")
 
 			let self = this
 			navigator.geolocation.getCurrentPosition( function(pos) {
-				console.log("HERE")
+				PostData('user/pushCoord', {uId: self.state.uId, longAllow: pos.coords.longitude, latAllow: pos.coords.latitude}).then ((result) => {
+				})
 				self.setState({
 					longAllow: pos.coords.longitude,
-					latAllow: pos.coords.latitude
+					latAllow: pos.coords.latitude,
+					// markers: {
+					// 	position: {
+					// 		lat: pos.coords.latitude,
+					// 		lng: pos.coords.longitude
+					// 	}
+					// }
 				})
 			})
 			axios.get('http://ip-api.com/json')
 			  .then(function(response){
+			  	PostData('user/pushCoord', {uId: self.state.uId, city: response.data.city, country: response.data.country, longDen: response.data.lon, latDen: response.data.lat}).then ((result) => {
+				})
 			  	self.setState({
 					longDen: response.data.lon,
 					latDen: response.data.lat,
 					city: response.data.city,
 					country: response.data.country
 				})
-			  })
-			PostData('user/pushCoord', {uId: self.uId, city: self.city, country: self.country, longAllow: self.longAllow, latAllow: self.latAllow, longDen: self.longDen, latDen: self.latDen}).then ((result) => {
-				console.log('in state', self)
-			})
+			})				 
+			
 		}
 	}
 
+    handleMapClick = this.handleMapClick.bind(this);
+    handleMapLoad = this.handleMapLoad.bind(this);
+    updateLocation = this.updateLocation.bind(this);
+
+    updateLocation(event){
+    	event.preventDefault()
+		    console.log("handle submit lat ", this.state.markers.position.lat());
+		    console.log("handle submit lng ", this.state.markers.position.lng());
+		const lat = this.state.markers.position.lat()
+		const lng = this.state.markers.position.lng()
+		PostData('user/pushCoord', {uId: this.state.uId, longAllow: lng, latAllow: lat}).then ((result) => {
+			console.log("after update coord: ", result)
+			// this.setState({
+			// 	markers: {
+			// 		position: {
+			// 			lat: result.latAllow,
+			// 			lng: result.lngAllow
+			// 		}
+			// 	}
+			// })
+		})
+		//пока город апдейтить не буду, до этого записан по айпи, но может быть впн, как и цмышленный выбор на карте себя в другой стране
+  //   	Geocode.fromLatLng(lat, lng).then(
+		//   response => {
+		//     const city = response.results[0].formatted_address.split(', ');
+
+		//     console.log("address: ", city);
+		//   },
+		//   error => {
+		//     console.error(error);
+		//   }
+		// );
+
+    }
+
+    handleMapClick(event) {
+        let that = this;
+        let mapRef = this._mapComponent;
+        this.setState({
+            markers: {
+                position: event.latLng,
+                key: Date.now()
+            },
+            mapCenter: event.latLng,
+            zoomLevel: mapRef.getZoom()
+        });
+
+    }
+
+    handleMapLoad(map) {
+        this._mapComponent = map;
+    }
+
 	render(){
-		console.log('LOCATION', this.state)
 		const lat = this.state.latAllow == '' ? this.state.latDen : this.state.latAllow
 		const lng = this.state.longAllow == '' ? this.state.longDen : this.state.longAllow
-
-		const MyGoogleMap = withGoogleMap(props => (
-			<GoogleMap 
-				defaultZoom={11}
-    			defaultCenter={{ lat: 50.469017, lng: 30.462191 }}>
-					<Marker
-      					position={{ lat: parseInt(props.lat), lng: parseInt(props.lng)}}
-    				/>
-    		</GoogleMap>
-    			))
+		const GoogleMapWrapper = withGoogleMap(props => (
+            <GoogleMap
+                ref={props.onMapLoad}
+                defaultZoom={props.zoomLevel}
+                defaultCenter={props.center}
+                onClick={props.onMapClick}
+            >
+                <Marker {...props.markers} />
+            </GoogleMap>
+        ))
 		return(
-			<form className="form" id="location2" style={{height: '700px'}}>
+			<form className="form" id="location2" style={{height: '700px'}} onSubmit={this.updateLocation}>
 				<div className="form-group">
 					<div className="col-xs-12">
 						<label htmlFor="location">
@@ -84,29 +194,19 @@ class Location extends Component {
 
 				<div style={{height: `100%`}}>
 					<div className="col-xs-12" style={{height: `100%`}}>
-
-					<MyGoogleMap
-						containerElement={
-							<div style={{    position: 'absolute',
-	                        top: 0,
-	                        left: 0,
-	                        right: 0,
-	                        bottom: 0,
-	                        justifyContent: 'flex-end',
-	                        alignItems: 'center',}} />
-						}
-						mapElement={
-							<div style={{    position: 'absolute',
-	                        top: 0,
-	                        left: 0,
-	                        right: 0,
-	                        bottom: 0,
-	                   }} />
-						}
-						lat={lat}
-						lng={lng}
-
-						/>	
+					<GoogleMapWrapper
+                    containerElement={
+                        <div style={{ height: `50vh` }} />
+                    }
+                    mapElement={
+                        <div style={{ height: `50vh` }} />
+                    }
+                    onMapClick={this.handleMapClick}
+                    onMapLoad={this.handleMapLoad}
+                    markers={this.state.markers}
+                    center={this.state.mapCenter}
+                    zoomLevel={this.state.zoomLevel}
+                	/>
 				</div>
 				</div>
 				</div>
