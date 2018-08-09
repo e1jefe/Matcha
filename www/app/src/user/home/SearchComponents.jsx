122392fbaa0514703159d2ab7caae94420722c9e
e1jefe <dmitry.sheptun@gmail.com>
2018-08-09 16:44:21 +0300

    slider

--- www/app/src/user/home/SearchComponents.jsx
+++ www/app/src/user/home/SearchComponents.jsx
@@ -9,6 +9,8 @@ import UsersCards from "./UsersCards";
 import jwtDecode from 'jwt-decode';
 import { PostData } from '../main/components/PostData';
 import {findDOMNode} from 'react-dom';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
 
 class SearchComponents extends Component {
     constructor(props) {
@@ -17,7 +19,8 @@ class SearchComponents extends Component {
             age: props.age,
             distance: props.distance,
             fameRate: props.fameRate,
-            res: ''
+            res: '',
+            value: { min: 18, max: 30 },
         }
         // this.onChange = this.onChange.bind(this);
     }
@@ -40,7 +43,8 @@ class SearchComponents extends Component {
     }
 
     onChange(event) {
-        console.log('event: ', event);
+        console.log('event: ', this.value);
+        console.log(this);
         // let age = findDOMNode(this.refs.age)
         // console.log('age: ', age);
 
@@ -60,11 +64,17 @@ class SearchComponents extends Component {
             <div id="wrapper1">
                 <div className="sliders">
                 <p>Age</p>
-                <Slider ref="age" id="age" min={18} range defaultValue={[18, 40]} onChange={this.onChange.bind(this)}/>
+                    <InputRange
+                        maxValue={55}
+                        minValue={18}
+                        value={this.state.value}
+                        onChange={value => this.setState({ value })} />
+
+                    {/*<Slider ref="age" id="age" min={18} range defaultValue={[18, 40]} onChange={this.onChange.bind(this)}/>*/}
                 <p>Distance</p>
-                <Slider name="distance"  min={2} defaultValue={30}  onChange={this.onChange}/>
+                {/*<Slider name="distance"  min={2} defaultValue={30}  onChange={this.onChange.bind(this)}/>*/}
                 <p>Fame rating</p>
-                <Slider name="fameRate" range step={10} defaultValue={[10, 50]} onChange={this.onChange}/>
+                {/*<Slider name="fameRate" range step={10} defaultValue={[10, 50]} onChange={this.onChange.bind(this)}/>*/}
                 </div>
                 <div className="tags">
                 <EditableTagGroup />
